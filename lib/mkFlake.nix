c2cc0971147d41f6a7db22c9af3be958569e1972
IogaMaster <iogamastercode@gmail.com>
2024-12-30 20:59:02 -0700

    feat(lib/mkFlake): add default to colmena nodes
    Add the `@all` tag, and allow local deployments

--- lib/mkFlake.nix
+++ lib/mkFlake.nix
@@ -1,6 +1,13 @@
 { inputs }:
 self: user-inputs: flake:
 {
-  colmena = (import ./mkColmena.nix inputs self);
+  colmena = (import ./mkColmena.nix inputs self) // {
+    defaults = {
+      deployment = {
+        tags = [ "all" ];
+        allowLocalDeployment = true;
+      };
+    };
+  };
 } // (inputs.blueprint { inputs = user-inputs; }) // flake
 
